f61ac13e88f2e96dfa1b58d42cdc7bb016c3050f
Ponk2System <ponk2system@gmail.com>
2026-05-21 14:22:14 +0700

    Fix : Input correction bug

--- Print Spooler.cpp
+++ Print Spooler.cpp
@@ -17,6 +17,11 @@ public:
     }
 
     void tambahDokumen(string namaDokumen){
+        if (namaDokumen.empty()){
+            cout << "!!! Nama dokumen tidak boleh kosong. Silakan coba lagi." << endl;
+            return;
+        }
+
         if (count == MAX_QUEUE_SIZE){
             cout << "Antrian penuh. Tidak dapat menambahkan dokumen. Tunggu printer selesai mencetak." << endl;
         } 
@@ -55,6 +60,22 @@ public:
     }
 };
 
+void inputPilihan(int &pilihan){;
+    //do{
+        cout << "Pilih opsi (1-4): ";
+        cin >> pilihan;
+        
+        if (cin.fail() || pilihan < 1 || pilihan > 4) {
+            cin.clear();  // Clear error flag
+            cin.ignore(10000, '\n');  // Clear buffer
+            //cout << "!!! Pilihan tidak valid! Silakan pilih antara 1-4." << endl;
+            //pilihan = 0;  // Reset pilihan
+            // Retry...
+        }
+    //} while (pilihan < 1 || pilihan > 4);
+    
+}
+
 int main(){
     PrintSpooler spooler;
     int pilihan;
@@ -66,13 +87,13 @@ int main(){
         cout << "2. Cetak Dokumen" << endl;
         cout << "3. Lihat Antrean" << endl;
         cout << "4. Keluar" << endl;
-        cout << "Pilih opsi (1-4): ";
-        cin >> pilihan;
+        inputPilihan(pilihan);
 
         switch (pilihan){
             case 1:
                 cout << "Masukkan nama dokumen: ";
-                cin >> doc;
+                cin.ignore(); //untuk mengabaikan space
+                getline(cin, doc);
                 spooler.tambahDokumen(doc);
                 break;
             case 2:
